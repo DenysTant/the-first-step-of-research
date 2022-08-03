02d3d08e46d89cbe4247d5f35f8f184f479b57e3
DenysTant <110553167+DenysTant@users.noreply.github.com>
2022-08-03 22:30:38 +0300

    Update car.py
    add tests

--- car.py
+++ car.py
@@ -1,7 +1,8 @@
+from pathlib import Path
 car = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
 }
 
 x = car.keys()
@@ -9,3 +10,22 @@ x = car.keys()
 car["color"] = "white"
 for name, value in car.items():
     print(name, value)
+
+a = '\#'
+if a != 0:
+    print(a)
+
+points = {
+    (0, 0): "O",
+    (1, 1): "A",
+    (2, 2): "B"
+}
+
+points[4] = 's'
+points[(3, 3)] = 's'
+print(points)
+
+
+p = Path()  # p Вказує на папку з якої був запущений Python
+print(p)
+print(type(p))
